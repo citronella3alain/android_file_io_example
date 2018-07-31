0939a11ca9d8c93eb5a369ff7e1719fab25fa262
citronella3alain <excursion.allen@gmail.com>
2018-07-31 15:16:00 -0700

    Update: doesn't overwrite existing logs (must provide distinct file names)

--- app/src/main/java/com/example/allen/fileioexample/MainActivity.java
+++ app/src/main/java/com/example/allen/fileioexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.allen.fileioexample;
 
+import android.content.Context;
 import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.*;
 
@@ -34,6 +36,21 @@ public class MainActivity extends AppCompatActivity {
 
         if (isExternalStorageWritable()){
             File file = new File(this.getExternalFilesDir(null), fileName.getText().toString() + ".txt");
+            if (file.exists()){
+                Log.i("tag","File has already been created");
+                Context context = getApplicationContext();
+                CharSequence text = "Error: File name already in existence";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                return;
+            }
+            Log.i("tag", "You have just created a new file");
+            Context context = getApplicationContext();
+            CharSequence text = "Writing to file...";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
             FileOutputStream fileOutput = new FileOutputStream(file, true);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutput);
             outputStreamWriter.write(msgBody.getText().toString());
